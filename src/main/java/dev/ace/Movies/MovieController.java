8977567883fedb5d411f195fac21be44e377945c
Malik Abdullah <abdmal573@gmail.com>
2024-06-05 23:57:30 +0500

    added new url address

--- src/main/java/dev/ace/Movies/MovieController.java
+++ src/main/java/dev/ace/Movies/MovieController.java
@@ -1,7 +1,10 @@
 package dev.ace.Movies;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class MovieController {
     @GetMapping
-    public String allMovies() {
-        return "All Movies!";
+    public ResponseEntity<String> allMovies() {
+        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
     }
 }
