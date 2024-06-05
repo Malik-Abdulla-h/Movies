c5111ce301bc246921bd2b902fd906c8a131b9ad
Malik Abdullah <abdmal573@gmail.com>
2024-06-06 00:30:24 +0500

    This will find all movies

--- src/main/java/dev/ace/Movies/MovieController.java
+++ src/main/java/dev/ace/Movies/MovieController.java
@@ -1,5 +1,6 @@
 package dev.ace.Movies;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 
 
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> allMovies() {
-        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getallMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovie(), HttpStatus.OK);
     }
 }
